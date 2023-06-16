f16c2f0f672f14956b4c16e9d4d5a6d9d6fff985
Dibyansh21 <nilaysingh33@gmail.com>
2023-06-16 13:32:50 +0530

    initial commit

---
+++ src/lib/apiList.js
@@ -0,0 +1,15 @@
+export const server = "http://localhost:4444";
+
+const apiList = {
+    login: `/auth/login`,
+    signup: `/auth/signup`,
+    uploadResume: `/upload/resume`,
+    uploadProfileImage: `/upload/profile`,
+    jobs: `/api/jobs`,
+    applications: `/api/applications`,
+    rating: `/api/rating`,
+    user: `/api/user`,
+    applicants: `/api/applicants`,
+};
+
+export default apiList;
